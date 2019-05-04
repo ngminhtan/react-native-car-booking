7078b848c3f310c3967e808624b30e61bb6a0c8b
TanNM <tannm@hamsa.co>
2019-05-04 10:54:16 +0700

    apply maps + native-base

--- src/routes/Home/components/Home.js
+++ src/routes/Home/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-
+import {MapContainer} from "./MapContainer";
+import { Container } from 'native-base'
 export default class Home extends React.Component{
 
     componentDidMount(): void {
@@ -8,10 +9,17 @@ export default class Home extends React.Component{
     }
 
     render(): React.ReactElement<any> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+        const region = {
+            latitude: 21.027763,
+            longitude: 105.834160,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+        }
+
         return (
-            <View >
-                <Text>{this.props.name}</Text>
-            </View>
+            <Container>
+                <MapContainer region={region} />
+            </Container>
         )
     }
 }
